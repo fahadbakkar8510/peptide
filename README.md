de4be303cc35656e397756fd3356b693ce18519f
fahadbakkar1998 And <Fahadbakkar48@gmail.com>
2022-09-15 14:08:28 -0500

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# Peptides
+
+Description
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <panel></panel>
+    <viewport></viewport>
+  </div>
+</template>
+
+<script>
+import ViewPort from "@/components/ViewPort.vue";
+import ControlPanel from "@/components/ControlPanel.vue";
+
+export default {
+  components: {
+    viewport: ViewPort,
+    panel: ControlPanel
+  }
+};
+</script>
+
+<style>
+html,
+body {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+body {
+  margin: 0px;
+}
+
+canvas {
+  position: relative;
+}
+
+#app {
+  height: 100%;
+}
+</style>
