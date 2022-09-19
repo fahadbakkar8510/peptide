0e46e08296c695e4f76168d07d4fa905512c3e9c
fahadbakkar1998 And <Fahadbakkar48@gmail.com>
2022-09-19 18:28:49 -0500

    apply instance texture to the sphere.

--- src/meshes.js
+++ src/meshes.js
@@ -12,92 +12,61 @@ import {
 import { getTextMaterial } from "./common";
 
 export const getAcidInstMesh = ({ radius, chars }) => {
-  const size = radius * 2;
-  const geometry = new BoxGeometry(size, size, size, 1, 1, 1);
+  const geometry = new SphereGeometry(radius);
 
-  const material = [
-    new MeshStandardMaterial({
-      map: new TextureLoader().load(
-        "https://threejs.org/examples/textures/square-outline-textured.png"
-      ),
-    }),
-    new MeshStandardMaterial({
-      map: new TextureLoader().load(
-        "https://threejs.org/examples/textures/golfball.jpg"
-      ),
-    }),
-    new MeshStandardMaterial({
-      map: new TextureLoader().load(
-        "https://threejs.org/examples/textures/metal.jpg"
-      ),
-    }),
-    new MeshStandardMaterial({
-      map: new TextureLoader().load(
-        "https://threejs.org/examples/textures/roughness_map.jpg"
-      ),
-    }),
-    new MeshStandardMaterial({
-      map: new TextureLoader().load(
-        "https://threejs.org/examples/textures/tri_pattern.jpg"
-      ),
-    }),
-    new MeshStandardMaterial({
-      map: new TextureLoader().load(
-        "https://threejs.org/examples/textures/water.jpg"
-      ),
-    }),
-  ];
+  const material = new MeshStandardMaterial({
+    map: new TextureLoader().load(
+      "https://threejs.org/examples/textures/square-outline-textured.png"
+    ),
+  });
 
-  material.forEach((m, side) => {
-    if (side != 2) return;
+  material.onBeforeCompile = (shader) => {
+    shader.uniforms.textures = {
+      type: "tv",
+      value: [
+        new TextureLoader().load(
+          "https://threejs.org/examples/textures/crate.gif"
+        ),
+        new TextureLoader().load(
+          "https://threejs.org/examples/textures/equirectangular.png"
+        ),
+        new TextureLoader().load(
+          "https://threejs.org/examples/textures/colors.png"
+        ),
+      ],
+    };
 
-    m.onBeforeCompile = (shader) => {
-      shader.uniforms.textures = {
-        type: "tv",
-        value: [
-          new TextureLoader().load(
-            "https://threejs.org/examples/textures/crate.gif"
-          ),
-          new TextureLoader().load(
-            "https://threejs.org/examples/textures/equirectangular.png"
-          ),
-          new TextureLoader().load(
-            "https://threejs.org/examples/textures/colors.png"
-          ),
-        ],
-      };
-
-      shader.vertexShader = shader.vertexShader
-        .replace(
-          "#define STANDARD",
-          `#define STANDARD
+    shader.vertexShader = shader.vertexShader
+      .replace(
+        "#define STANDARD",
+        `#define STANDARD
                   varying vec3 vTint;
                   varying float vTextureIndex;`
-        )
-        .replace(
-          "#include <common>",
-          `#include <common>
+      )
+      .replace(
+        "#include <common>",
+        `#include <common>
               attribute vec3 tint;
               attribute float textureIndex;`
-        )
-        .replace(
-          "#include <project_vertex>",
-          `#include <project_vertex>
+      )
+      .replace(
+        "#include <project_vertex>",
+        `#include <project_vertex>
               vTint = tint;
               vTextureIndex=textureIndex;`
-        );
+      );
 
-      shader.fragmentShader = shader.fragmentShader
-        .replace(
-          "#define STANDARD",
-          `#define STANDARD
+    shader.fragmentShader = shader.fragmentShader
+      .replace(
+        "#define STANDARD",
+        `#define STANDARD
                   uniform sampler2D textures[3];
                   varying vec3 vTint;
                   varying float vTextureIndex;`
-        )
-        .replace(
-          "#include <fog_fragment>",
-          `#include <fog_fragment>
+      )
+      .replace(
+        "#include <fog_fragment>",
+        `#include <fog_fragment>
               float x = vTextureIndex;
               vec4 col;
               col = texture2D(textures[0], vUv ) * step(-0.1, x) * step(x, 0.1);
@@ -105,9 +74,8 @@ export const getAcidInstMesh = ({ radius, chars }) => {
               col += texture2D(textures[2], vUv ) * step(1.9, x) * step(x, 2.1);
               gl_FragColor = col;
               `
-        );
-    };
-  });
+      );
+  };
 
   const mesh = new InstancedMesh(geometry, material, chars.length);
   const textures = [];
