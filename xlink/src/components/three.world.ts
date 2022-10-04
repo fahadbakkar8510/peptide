2f43033f5790767df769cde71ad62e4a650a253a
fahadbakkar1998 And <Fahadbakkar48@gmail.com>
2022-10-04 14:15:29 -0500

    update terrain vertices var name.

--- xlink/src/components/three.world.ts
+++ xlink/src/components/three.world.ts
@@ -84,10 +84,10 @@ export class ThreeWorld implements ThreeInterface {
     const heightData = generateHeight(terrainWidth, terrainDepth, terrainMinHeight, terrainMaxHeight);
     const terrainGeometry = new THREE.PlaneGeometry(terrainWidthExtents, terrainDepthExtents, terrainWidth - 1, terrainDepth - 1);
     terrainGeometry.rotateX(- Math.PI / 2);
-    const vertices = terrainGeometry.attributes.position.array;
-    for (let i = 0, j = 0, l = vertices.length; i < l; i++, j += 3) {
+    const terrainVertices = terrainGeometry.attributes.position.array;
+    for (let i = 0, j = 0, l = terrainVertices.length; i < l; i++, j += 3) {
       // j + 1 because it is the y component that we modify
-      vertices[j + 1] = heightData[i];
+      terrainVertices[j + 1] = heightData[i];
     }
     terrainGeometry.computeVertexNormals();
     const groundMaterial = new THREE.MeshPhongMaterial({ color: 0xC7C7C7 });
